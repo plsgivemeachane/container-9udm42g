ae667ba5a5c9d6969ae9d1f58752e6d78ddf4786
plsgivemeachane <100744636+plsgivemeachane@users.noreply.github.com>
2023-12-05 16:39:25 +0700

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-echo "Do I run this code???"
+#echo "Do I run this code???"
 
 set -ex
 
-# Start gunicorn, listening on port 500, access log to stdout
-exec gunicorn -w 4 -b '0.0.0.0:5000' --access-logfile=- 'app:app'
+gunicorn -w 4 -b '0.0.0.0:5000' --access-logfile=- 'app:app'
